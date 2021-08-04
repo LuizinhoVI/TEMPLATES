2ff7050f19ccfe75932fcb4329409aa815382d75
LUIZ VIEIRA <beat.luiz23@hotmail.com>
2021-08-04 07:33:48 -0300

    att

--- action.js
+++ action.js
@@ -1,4 +1,18 @@
-function juntar() {
-    var mensagem = document.getElementById('nome');
-    mensagem.innerHTML.style = "none";
+var i = 1;
+
+
+function myFunction(x) {
+    var fundo = document.getElementById("cabeca");
+    var itens = document.getElementById("ul");
+    x.classList.toggle("change");
+    fundo.style.height = "100%";
+    itens.style.display = "block"
+    if (i == 2) {
+        fundo.style.height = "50px";
+        fundo.style.width = "100%";
+        itens.style.display = "none"
+        i = 0;
+    }
+    i++;
+    console.log(i);
 }
